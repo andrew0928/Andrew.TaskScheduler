7580a03bf4323401bc144cf9eb53c040fa050536
Andrew Wu <chicken@chicken-house.net>
2019-08-28 01:19:51 +0800

    complete poc

--- TaskSchedulerWorker/Program.cs
+++ TaskSchedulerWorker/Program.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Concurrent;
-//using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace TaskSchedulerWorker
 {
@@ -9,147 +12,152 @@ namespace TaskSchedulerWorker
     {
         static void Main(string[] args)
         {
-            var p = new Program();
-            
-        }
-
-
-
-
-
-
-        private int _workers_count = 5;
-
-        private ConcurrentQueue<TaskInfo> _tasks = new ConcurrentQueue<TaskInfo>();
-        private AutoResetEvent[] _waits = null;
-        private ManualResetEvent _reset_wait = new ManualResetEvent(false);
-        private Thread[] _threads = null;
-
-        public Program()
-        {
-
-            //int workers = 5;
-            this._threads = new Thread[this._workers_count];
-            this._waits = new AutoResetEvent[_workers_count];
-            for (int i = 0; i < _workers_count; i++)
-            {
-                _threads[i] = new Thread(this.ProcessTasksWorker);
-                this._waits[i] = new AutoResetEvent(false);
-
-                _threads[i].Start(this._waits[i]);
-            }
-
-            Thread update = new Thread(this.UpdateTasksWorker);
-            update.Start();
-        }
-
-
-        private void ProcessTasksWorker(object _wait)
-        {
-            AutoResetEvent wait = (AutoResetEvent)_wait;
-
-            while (true)
-            {
-                while (true)
+            new HostBuilder()
+                .ConfigureServices((context, services) =>
                 {
-                    TaskInfo task;
-                    // try get task from queue
-                    while (this._tasks.TryDequeue(out task) == false)
-                    {
-                        //Console.WriteLine("dequeue fail, waiting...");
-                        wait.WaitOne();
-                        //Console.WriteLine("dequeue fail, waked up...");
-                    }
+                    services.AddSingleton<TaskInfoCollection>();
 
-                    // wait until task should run
-                    bool skip = false;
-                    while (DateTime.Now < task.RunAt)
-                    {
-                        //if (wait.WaitOne(task.RunAt - DateTime.Now) == true) continue;
-
-                        int result = WaitHandle.WaitAny(
-                            new WaitHandle[] { wait, _reset_wait },
-                            task.RunAt - DateTime.Now);
-                        //Console.WriteLine(result);
-                        if (result == 0) continue;
-                        if (result == 1) { skip = true; break; }
-                    }
-
-                    if (skip == false)
-                    {
-                        //try
-                        //{
-                            if (task.Do())
-                            {
-                                // run
-                                Console.WriteLine($"Task: {task.Name}, Delay: {(DateTime.Now - task.RunAt).TotalMilliseconds} msec, RunAt: {task.RunAt}, Current: {DateTime.Now}");
-                            }
-                            //else
-                            //{
-                            //    // try run fail
-                            //    Console.WriteLine("...");
-                            //}
-                        //}
-                        //catch
-                        //{
-                        //    Console.WriteLine("___");
-                        //}
-                    }
-                }
-            }
+                    services.AddHostedService<DispatchWorker>();
+                    services.AddHostedService<FetchWorker>();
+                })
+                .RunConsoleAsync()
+                .Wait();
         }
 
-        private void UpdateTasksWorker()
+    }
+
+    public class FetchWorker : BackgroundService
+    {
+        private TaskInfoCollection _tasks = null;
+
+        public FetchWorker(TaskInfoCollection tasks)
+        {
+            this._tasks = tasks;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             DateTime basetime = DateTime.Now;
-            for(int count = 1; count < 10000; count++)
+
+            for (int count = 1; count < 10000; count++)
             {
-                TaskInfo task = new TaskInfo() { Name = $"Task#{count}", RunAt = basetime.AddMilliseconds(1200 * count) };
-                this._tasks.Enqueue(task);
-                foreach (var wait in this._waits) wait.Set();
+                if (stoppingToken.IsCancellationRequested) break;
+
+                TaskInfo task = new TaskInfo()
+                {
+                    Name = $"Task#{count}",
+                    RunAt = basetime.AddMilliseconds(1200 * count)
+                };
+
+                this._tasks.Set(task);
                 Console.WriteLine($"Enque Job: {task.Name} (@{task.RunAt})");
 
-                Thread.Sleep(900);
+                await Task.Delay(900);
 
-                if (count % 20 == 0)
+                if (count % 10 == 0)
                 {
-                    _reset_wait.Set();
                     this._tasks.Clear();
-                    _reset_wait.Reset();
-
-                    //foreach (var wait in this._waits) wait.Set();
                     Console.WriteLine($"Clean Queue");
                 }
             }
 
+            this._tasks.Complete();
+        }
 
-            //while (true)
-            //{
-            //    Console.WriteLine("Reset Queue...");
-            //    this._tasks.Clear();
-            //    foreach (var wait in this._waits) wait.Set();
+    }
 
-            //    this._tasks = new ConcurrentQueue<TaskInfo>(new TaskInfo[] {
-            //        new TaskInfo() { Name = "01", RunAt = DateTime.Now.AddMilliseconds(1500) },
-            //        new TaskInfo() { Name = "02", RunAt = DateTime.Now.AddMilliseconds(2000) },
-            //        new TaskInfo() { Name = "03", RunAt = DateTime.Now.AddMilliseconds(2500) },
-            //        new TaskInfo() { Name = "04", RunAt = DateTime.Now.AddMilliseconds(3000) },
-            //        new TaskInfo() { Name = "05", RunAt = DateTime.Now.AddMilliseconds(3500) },
-            //        new TaskInfo() { Name = "06", RunAt = DateTime.Now.AddMilliseconds(4000) },
-            //        new TaskInfo() { Name = "07", RunAt = DateTime.Now.AddMilliseconds(4500) },
-            //        new TaskInfo() { Name = "08", RunAt = DateTime.Now.AddMilliseconds(5000) },
-            //        new TaskInfo() { Name = "09", RunAt = DateTime.Now.AddMilliseconds(5500) },
-            //        new TaskInfo() { Name = "10", RunAt = DateTime.Now.AddMilliseconds(6000) },
-            //    });
-            //    foreach (var wait in this._waits) wait.Set();
+    public class DispatchWorker : BackgroundService
+    {
+        private TaskInfoCollection _tasks = null;
+        public DispatchWorker(TaskInfoCollection tasks)
+        {
+            this._tasks = tasks;
+        }
 
-            //    Thread.Sleep(5000);
-            //}
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (stoppingToken.IsCancellationRequested == false)
+            {
+                TaskInfo task = await this._tasks.GetAsync();
+
+                if (task == null) continue;
+
+
+                if ((DateTime.Now < task.RunAt) && await this._tasks.WaitClearAsync(task.RunAt - DateTime.Now))
+                {
+                    // reset, abort current task, reget & redo
+                    continue;
+                }
+
+                if (task.Do())
+                {
+                    // get lock, run
+                    Console.WriteLine($"Task: {task.Name}, Delay: {(DateTime.Now - task.RunAt).TotalMilliseconds} msec, RunAt: {task.RunAt}, Current: {DateTime.Now}");
+                }
+            }
+        }
+    }
+
+
+    public class TaskInfoCollection
+    {
+        private ConcurrentQueue<TaskInfo> _tasks = new ConcurrentQueue<TaskInfo>();
+        private AutoResetEvent _wait = new AutoResetEvent(false);
+        private ManualResetEvent _reset = new ManualResetEvent(false);
+        private WaitHandle[] _all_waits = null;
+
+        public TaskInfoCollection()
+        {
+            this._all_waits = new WaitHandle[]
+            {
+                this._wait,
+                this._reset
+            };
+        }
+
+        public async Task<TaskInfo> GetAsync()
+        {
+            TaskInfo task;
+            while(this._tasks.TryDequeue(out task) == false)
+            {
+                if (await this.WaitNotifyAsync() == 1) return null;
+            }
+            return task;
+        }
+
+        private object _syncroot = new object();
+
+        public void Set(TaskInfo task)
+        {
+            lock (this._syncroot) this._tasks.Enqueue(task);
+            this._wait.Set();
+        }
+
+        public void Clear()
+        {
+            this._reset.Set();
+            lock (this._syncroot) this._tasks.Clear(); /* add items in atom operation */
+            this._reset.Reset();
+        }
+
+        public void Complete()
+        {
+            this._reset.Set();
+            lock (this._syncroot) this._tasks.Clear();
         }
 
 
+        // return 0: enqueue
+        // return 1: clean
+        private async Task<int> WaitNotifyAsync()
+        {
+            return await Task.Run<int>(() => { return WaitHandle.WaitAny(this._all_waits); });
+        }
 
-
+        public async Task<bool> WaitClearAsync(TimeSpan duration)
+        {
+            return await Task.Run<bool>(() => { return this._reset.WaitOne(duration); });
+        }
     }
 
 
@@ -174,9 +182,8 @@ namespace TaskSchedulerWorker
             }
 
             // do...
-            Thread.Sleep(100);
 
-            lock(this._syncroot)
+            lock (this._syncroot)
             {
                 if (this._state != 1) throw new InvalidOperationException();
                 this._state = 2;
@@ -185,4 +192,6 @@ namespace TaskSchedulerWorker
             return true;
         }
     }
+
+
 }
